5c8b340d077ee106ad04476e141159dfb29d6792
unknown <unknown@unknown.com>
2015-12-16 14:29:24 +0000

    functional

--- comix-server.go
+++ comix-server.go
@@ -8,13 +8,21 @@ import (
 	"flag"
 	"fmt"
 	//"github.com/goware/urlx"
+	"archive/zip"
+	"io"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
 	"strconv"
+	"strings"
 )
+
+var imageExt = []string{"jpg", "gif", "png", "tif", "bmp", "jpeg", "tiff"}
+var hiddenFullname = []string{".", "..", "@eaDir", "Thunmbs.db", ".DS_Store"}
+var hiddenPartname = []string{"__MACOSX"}
 
 type AppContext struct {
 	root string
@@ -30,19 +38,226 @@ func (h ContextHandlerFunc) ServeHTTPContext(ctx *AppContext, rw http.ResponseWr
 	h(ctx, rw, req)
 }
 
-func listDir(relPath string, w http.ResponseWriter) {
+func listDir(fullpath string, w http.ResponseWriter) {
+
+	log.Println("list dir " + fullpath)
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "jjjjjjjjjdas")
+
+	walkfunc := func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if path != fullpath {
+
+			fmt.Fprintln(w, info.Name())
+		}
+
+		return nil
+	}
+
+	filepath.Walk(fullpath, walkfunc)
+}
+
+func isInZip(path string, ext string) bool {
+	lowpath := strings.ToLower(path)
+
+	if strings.Index(lowpath, "zip") == -1 && strings.Index(lowpath, "cbz") == -1 {
+		return false
+	}
+
+	return ext != "zip" && ext != "cbz"
+
+}
+
+func processFileInZip(appcontex *AppContext,
+	w http.ResponseWriter, fullpath string, typestr string) {
+
+	zippos := strings.Index(fullpath, "zip")
+	if zippos == -1 {
+		zippos = strings.Index(fullpath, "cbz")
+	}
+
+	zipfilepath := fullpath[0 : zippos+3]
+
+	log.Println("zip file path:" + zipfilepath)
+
+	imagepath := strings.Replace(fullpath, zipfilepath+"/", "", -1)
+
+	log.Println("image path :" + imagepath)
+
+	reader, err := zip.OpenReader(zipfilepath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer reader.Close()
+
+	for _, f := range reader.File {
+
+		fi := f.FileInfo()
+
+		if fi.Name() == imagepath {
+
+			log.Println("found image " + imagepath)
+			w.Header().Add("Content-Type", typestr)
+			w.Header().Add("Content-Length", strconv.FormatInt(fi.Size(), 10))
+			rc, err := f.Open()
+			if err != nil {
+				log.Println(err)
+			}
+			defer rc.Close()
+
+			_, err = io.Copy(w, rc)
+			if err != nil {
+				log.Println(err)
+			}
+
+		}
+
+	}
+
+}
+
+func processImage(appcontex *AppContext,
+	w http.ResponseWriter, path string, imageExt string) {
+}
+
+func isSupport(filename string, isdir bool) bool {
+
+	if filename[0:1] == "." {
+		return false
+	}
+
+	if inArray(filename, hiddenFullname) {
+		return false
+	}
+
+	for _, p := range hiddenPartname {
+		if strings.Contains(filename, p) {
+
+		}
+	}
+
+	return true
+}
+
+func processZip(appcontex *AppContext,
+	w http.ResponseWriter, fullpath string) {
+
+	log.Printf("process zip %s\n", fullpath)
+
+	if fullpath[len(fullpath)-1:] == "/" {
+		fullpath = fullpath[0 : len(fullpath)-1]
+		log.Printf("as %s\n", fullpath)
+	}
+
+	reader, err := zip.OpenReader(fullpath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer reader.Close()
+
+	for _, f := range reader.File {
+
+		fi := f.FileInfo()
+		if isSupport(fi.Name(), fi.IsDir()) {
+
+			fmt.Fprintln(w, fi.Name())
+		}
+
+	}
+
+}
+
+func getContentType(ext string) string {
+	switch ext {
+	case "jpg":
+		return "image/jpeg"
+	case "tif":
+		return "image/tiff"
+	default:
+		return "image/" + "ext"
+	}
+}
+
+func inArray(check string, array []string) bool {
+
+	for _, inarray := range array {
+		if check == inarray {
+			return true
+		}
+	}
+	return false
+}
+
+func isDir(fullpath string) bool {
+
+	file, err := os.Open(fullpath)
+	if err != nil {
+
+		log.Println(err)
+		return false
+	}
+	defer file.Close()
+
+	fileinfo, err := file.Stat()
+	if err != nil {
+		log.Fatalln(err)
+		return false
+	}
+
+	return fileinfo.IsDir()
+
 }
 
 func viewHandler(ctx *AppContext, w http.ResponseWriter, r *http.Request) {
-	log.Println("request is '" + r.URL.String() + "', after normailze :'")
+	log.Println("request is '" + r.URL.String())
 
 	fullpath := path.Join(ctx.root, r.URL.Path)
 
 	//listDir(r.URL.Path[len("/"):], w)
-	fmt.Fprintf(w, "url path %s, after join: %s", r.URL.Path, fullpath)
+	log.Printf("url path %s, after join: %s\n", r.URL.Path, fullpath)
+
+	if isDir(fullpath) {
+		log.Printf("%s is dir \n", fullpath)
+		listDir(fullpath, w)
+		return
+	}
+
+	log.Printf("%s is not dir \n", fullpath)
+
+	ext := filepath.Ext(fullpath)
+	if len(ext) >= 1 {
+
+		ext = ext[1:]
+	}
+
+	log.Printf("ext of %s is %s", fullpath, ext)
+
+	typestr := getContentType(ext)
+
+	log.Printf("type of %s is %s", fullpath, typestr)
+
+	if isInZip(fullpath, ext) {
+		processFileInZip(ctx, w, fullpath, typestr)
+		return
+	}
+
+	if inArray(ext, imageExt) {
+		processImage(ctx, w, fullpath, typestr)
+		return
+	}
+
+	if ext == "zip" || ext == "cbz" {
+		processZip(ctx, w, fullpath)
+		return
+	}
+
 }
 
 type ContextAdapter struct {
